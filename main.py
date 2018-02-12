8b2b9ff7f1f76d8e361d39631e24285a3739e29b
Alex Shevchenko <alexshevchenko9999@gmail.com>
2018-02-11 21:25:35 -0500

    Lines

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(0,0,500,450,screen,color)
+draw_line(250,250,500,450,screen,color)
 draw_line(250,250,400,500,screen,color)
 draw_line(250,250,250,500,screen,color)
-draw_line(450,500,250,250,screen,color)
+draw_line(0,500,250,250,screen,color)
+draw_line(0,250,500,250,screen,color)
 
 display(screen)
 save_extension(screen, 'img.png')
